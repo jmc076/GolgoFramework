e09b9868df93d04cc88bf61ba2fc65b86d0bd02a
Diego Lopez <dlopezr@viewnext.com>
2017-03-07 12:30:15 +0100

    session controller updates

---
+++ Private/Controllers/GFSessions/CSRFChecker.php
@@ -0,0 +1,50 @@
+<?php
+namespace Controllers\GFSessions;
+
+use Helpers\HelperUtils;
+
+class CSRFChecker {
+
+	const CSRF_SCOPE = "gf_csrf";
+	private $session;
+
+	public function __construct(){
+		$sessionController = new SessionController(self::CSRF_SCOPE);
+		$this->session = $sessionController->getSession();
+	}
+
+	public function isValid($dataArray) {
+		if(isset($dataArray[$this->getTokenId()])) {
+			if ($dataArray[$this->getTokenId()] == $this->getTokenValue()) {
+				return true;
+			} else {
+
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	public function getTokenId() {
+
+		if(isset($this->session['token_id'])) {
+			return $this->session['token_id'];
+		} else {
+			$token_id = HelperUtils::getRandomKey(10);
+			$this->session['token_id'] = $token_id;
+			return $token_id;
+		}
+	}
+
+	public function getTokenValue() {
+		if(isset($this->session['token_value'])) {
+			return $this->session['token_value'];
+		} else {
+			$token = hash('sha256', HelperUtils::getRandomKey(500));
+			$this->session['token_value'] = $token;
+			return $token;
+		}
+
+	}
+}
--- Private/Controllers/GFSessions/SessionController.php
+++ Private/Controllers/GFSessions/SessionController.php
@@ -1,141 +1,94 @@
 <?php
-namespace Controllers;
+namespace Controllers\GFSessions;
 
-use Helpers\HelperUtils;
-use Controllers\Interfaces\SessionControllerInterface;
 
 class SessionController {
-	
-	public function initSession() {
-		@session_start();
-		
-		$sessionContainer = array(
-				'user_model' => SESSION_ANONYM,
-				'user_id' => 0,
-				'user_ip' => self::getIp(),
-				'session_name' => SESSION_NAME,
-				'user_cart' => array(),
-				'user_extra' => array(),
-				'status' => false,
-				
-		);
-		
-		if(!isset($_SESSION["sessionData"])) {
-			$_SESSION["sessionData"] = $sessionContainer;
-		}
-		
-		self::initSessionTimings();
-		self::get_token_id();
-		self::get_token();
+
+	const GF_GLOBAL_SESSION = "gf_global";
+
+	private $scope;
+
+	public function __construct($scope = self::GF_GLOBAL_SESSION) {
+		$this->scope = $scope;
 	}
-	
-	public static function getIp()
-	{
-		if(isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != '') {
-		   return $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+	public function getSession(){
+		if(isset($_SESSION[$this->scope])) {
+			return $_SESSION[$this->scope];
 		} else {
-		   return $_SERVER['REMOTE_ADDR'];
+			$_SESSION[$this->scope] = array();
+			return $_SESSION[$this->scope];
 		}
 	}
-	
-	
-	public static function regenerateSession() {
-		session_regenerate_id(true);
-		$_SESSION['CREATED'] = time();
+
+
+	public function isSetScope($scope) {
+		return isset($_SESSION[$scope]);
 	}
-	
-	public static function getSessionId() {
-		return session_id();
-	}
-	
-	public static function setSessionId($sessionID) {
-		session_id($sessionID);
-	}
-	
-	public static function setSessionKeyValue($key, $value = null) {
-		$_SESSION['sessionData'][$key] = $value;
-	}
-	public static function getSessionValue($key) {
-		return isset($_SESSION['sessionData'][$key]) ? $_SESSION['sessionData'][$key] : null;
-	}
-	
-	public static function exitSession() {
-		$_SESSION = array();
-		if (ini_get("session.use_cookies")) {
-		    $params = session_get_cookie_params();
-		    setcookie(session_name(), '', time() - 42000,
-		        $params["path"], $params["domain"],
-		        $params["secure"], $params["httponly"]
-		    );
+
+	public function put($key, $value) {
+		try {
+			$session = $this->getSessionScope();
+			$session[$key] = $value;
+		} catch (Exception $e) {
+			return false;
 		}
-		@session_unset();
-		@session_destroy();
+		return true;
+
 	}
-	
-	public static function expireSession() {
-		$_SESSION = array();
-		$_SESSION["oldSessionRedirect"] = $_SERVER['REQUEST_URI'];
-		@session_unset();
-		@session_destroy();
-	}
-	
-	public static function initSessionTimings(){
-		if (!isset($_SESSION['CREATED'])) {
-			$_SESSION['CREATED'] = time();
-		} else if (time() - $_SESSION['CREATED'] > SESSION_LENGTH) {
-			self::regenerateSession();
-		}
-		if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > SESSION_LENGTH)) {
-			self::expireSession();
-		}
-		
-		$_SESSION['LAST_ACTIVITY'] = time();
-	}
-	
-	
-	public static function getCurrentUserModel() {
-		$userModel = $_SESSION["sessionData"]["user_model"];
-		if (class_exists($userModel)) {
-			$model = new $userModel();
-			return $model;
-		} else {
-			$user = SESSION_ANONYM;
-			$model = new $user();
-			return $model;
-		}
-	}
-	
-	public static function get_token_id() {
-		if(isset($_SESSION['token_id'])) {
-			return $_SESSION['token_id'];
-		} else {
-			$token_id = HelperUtils::getRandomKey(10);
-			$_SESSION['token_id'] = $token_id;
-			return $token_id;
-		}
-	}
-	public static function get_token() {
-		if(isset($_SESSION['token_value'])) {
-			return $_SESSION['token_value'];
-		} else {
-			$token = hash('sha256', HelperUtils::getRandomKey(500));
-			$_SESSION['token_value'] = $token;
-			return $token;
-		}
-	
-	}
-	//FOR CSRF
-	public static function check_valid($dataArray, $exception) {
-		if(isset($dataArray[self::get_token_id()])) {
-			if ($dataArray[self::get_token_id()] == self::get_token()) {
+	public function safePut($key, $value){
+		try {
+			$session = $this->getSessionScope();
+			if(!isset($session[$key])){
+				$session[$key] = $value;
 				return true;
-			} else {
-				
-				$exception->invalidCSRF();
 			}
-		} else {
-			$exception->missingCSRF();
+			return false;
+
+		} catch (Exception $e) {
+			return false;
 		}
 	}
-	
+
+	public function get($key) {
+		try {
+			$session = $this->getSessionScope();
+			return isset($session[$key]) ? $session[$key]: null;
+		} catch (Exception $e) {
+			return null;
+		}
+	}
+
+	public function getOnce($key){
+		try {
+			$session = $this->getSessionScope();
+			$value = isset($session[$key]) ? $session[$key]: null;
+			$this->delete($key);
+			return $value;
+		} catch (Exception $e) {
+			return null;
+		}
+	}
+
+	public function delete($key) {
+		try {
+			$session = $this->getSessionScope();
+			unset($session[$key]);
+		} catch (Exception $e) {
+			return false;
+		}
+		return  true;
+	}
+
+
+	public function getScope() {
+		return $this->scope;
+	}
+	public function setScope($scope) {
+		$this->scope = $scope;
+		return $this;
+	}
+
+
+
 }
